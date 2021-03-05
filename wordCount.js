88b4e7ef93369db3bf130cdd09f81cfb6d6ef344
Lily Kong <lilyhk28@gmail.com>
2021-03-05 20:19:19 +0100

    mod wordCount

--- wordCount.js
+++ wordCount.js
@@ -17,7 +17,7 @@ countWords((err, data) => {
   //return data
 })
 
-let a = 'Before becoming a software engineer, I worked in corporate retail. My first position after college was for a small lighting company in Atlanta where I was an analyst reporting directly to the president of the company. That position was followed by roles working for a few well known Bay Area retailers managing sales plans for departments upwards of $150m in retail sales. My experience first working for a small company (roughly 50 employees) could not have prepared me better for the roles I later filled. I had the opportunity to wear many hats and learn how various departments within a company work together. While working at larger companies, I gained experience working cross-functionally with merchants, marketing, and production teams to develop buy-strategies and promo plans, and pitching these plans to directors and VPs on a weekly basis. My job not only required analyzing and interpreting data, it also focused heavily on problem solving and team work, both with other functions and fellow planners. My work had its own exciting challenges but I knew I wanted to tackle something new and different. I also know that my skills gained over the years, both soft and analytical, will not go to waste'
+
 
 
 
